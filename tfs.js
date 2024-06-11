954f18b6bc68abd3af5c30236700b4f86a38d1ec
Anjithsaju <anjithsaju123@gmail.com>
2024-06-12 00:08:51 +0530

    Add files via upload

---
+++ tfs.js
@@ -0,0 +1,9 @@
+
+const btn1=document.getElementById('b1');
+btn1.addEventListener('click',e=>{location.replace('tfs.html');});
+const btn2=document.getElementById('b2');
+btn2.addEventListener('click',e=>{location.replace('tfsgall.html');});
+const btn3=document.getElementById('b3');
+btn3.addEventListener('click',e=>{location.replace('tfscon.html');});
+const btn4=document.getElementById('b4');
+btn4.addEventListener('click',e=>{location.replace('tfsabout.html');});
